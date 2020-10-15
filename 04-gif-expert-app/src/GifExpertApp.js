3bd2cda43fecd8242d94abcf87d13807e5638f63
LazaroBC <bellochlazaro.smr@gmail.com>
2020-10-15 18:50:07 +0200

    Custom Hooks

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+// Para obligar a poner los componentes en <AddCategory />
+import PropTypes from 'prop-types'
+import { GifGrid } from './components/GifGrid'
+const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Samurai combat']);
+
+    // const handleAdd = () => {
+    //     setCategories(['1ºHunterXHunter', ...categories, '2ºHunterXHunter']);
+    //     // setCategories(cats=>[...cats,'NuevoPersonaje']);
+    // }
+
+    return (
+
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+
+            <ol>
+                {
+                    categories.map(category => (
+
+                        <GifGrid
+                            key={category}
+                            category={category} />
+                    ))
+                }
+            </ol>
+
+
+
+        </>
+    )
+
+
+
+}
+
+// Para obligar a poner los componentes en <AddCategory />
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
+
+
+export default GifExpertApp;
