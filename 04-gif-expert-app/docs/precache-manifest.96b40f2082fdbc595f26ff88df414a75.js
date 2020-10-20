55d0626d69a716b3949ffece9811929d7521ab73
LazaroBC <bellochlazaro.smr@gmail.com>
2020-10-20 09:19:47 +0200

    docs agregado

---
+++ 04-gif-expert-app/docs/precache-manifest.96b40f2082fdbc595f26ff88df414a75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20680d5062224b4adbfe8937618bd53f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22d00793ba3cb8805be1",
+    "url": "/static/css/main.a8632966.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d00793ba3cb8805be1",
+    "url": "/static/js/main.0538fa36.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
