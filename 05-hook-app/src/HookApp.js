75991f197500045e542d03097c5592b95ac41be0
LazaroBC <bellochlazaro.smr@gmail.com>
2020-10-23 08:36:35 +0200

    Hooks

---
+++ 05-hook-app/src/HookApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const HookApp = () => {
+
+    return ( 
+        <>
+            <div>
+                <h1>Hola mundo</h1>
+            </div>
+        </>
+    )
+}
